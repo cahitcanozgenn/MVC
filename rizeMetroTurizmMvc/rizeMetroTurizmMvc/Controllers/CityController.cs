0a55d218cd08a8a65e2e19351b8e7b77b4161c64
cahitcanozgenn <cahitcanozgen.dev@gmail.com>
2022-04-26 23:51:38 +0300

    RizeMetroTurizmMVC

---
+++ rizeMetroTurizmMvc/rizeMetroTurizmMvc/Controllers/CityController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using rizeMetroTurizmMvc.Models.Entity;
+
+namespace rizeMetroTurizmMvc.Controllers
+{
+    public class CityController : Controller
+    {
+        // GET: City
+        rizemetroturizmdbEntities1 db = new rizemetroturizmdbEntities1();
+        public ActionResult Index()
+        {
+            var values = db.city.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ rizeMetroTurizmMvc/rizeMetroTurizmMvc/Controllers/DegreeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using rizeMetroTurizmMvc.Models.Entity;
+
+namespace rizeMetroTurizmMvc.Controllers
+{
+    public class DegreeController : Controller
+    {
+        // GET: Degree
+        rizemetroturizmdbEntities1 db = new rizemetroturizmdbEntities1();
+        public ActionResult Index()
+        {
+            var values = db.degree.ToList();
+            return View(values);
+        }
+    }
+}
