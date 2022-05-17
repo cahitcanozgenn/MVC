b29bb6925cca0069a2b65410497b832ee5e70614
cahitcanozgenn <cahitcanozgen.dev@gmail.com>
2022-05-17 15:31:13 +0300

    MvcProject

--- MvcProject/MvcProject/Controllers/ProductController.cs
+++ MvcProject/MvcProject/Controllers/ProductController.cs
@@ -51,13 +51,30 @@ namespace MvcProject.Controllers
         public ActionResult productList(int id)
         {
             var product = db.product.Find(id);
+
+
+
+
+            List<SelectListItem> values = (from i in db.category.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.categoryName,
+                                               Value = i.categoryId.ToString()
+                                           }).ToList();
+            ViewBag.values = values; // diğer sayfaya değer taşıma
+
+
+
+
             return View("productList", product);
+
         }
         public ActionResult Update(product product)
         {
             var products = db.product.Find(product.productId);
             products.productName = product.productName;
-            products.productCategory = product.productCategory;
+            var category = db.category.Where(m => m.categoryId == product.category.categoryId).FirstOrDefault();
+            products.productCategory = category.categoryId;
             products.productUnitPrice = product.productUnitPrice;
             db.SaveChanges();
             return RedirectToAction("Index");
